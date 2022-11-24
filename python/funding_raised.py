bf8f222416c26d8fb31b13e7bfad499352925341
Franck <franck.somarribarodriguez@slu.edu>
2022-11-24 13:54:11 -0600

    dataframe was defined globaly, comments for the class and the where function have been added, where function has been rewrote using pandas and condensed

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -1,143 +1,152 @@
-import csv
+import pandas as pd
 
 class FundingRaised:
+  """
+  Returns company's information
+  """
+  global df
+  df = pd.read_csv('startup_funding.csv')
+  
   @staticmethod
   def where(options = {}):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+    """
+    Filters the dataframe based on the parameters provided, and returns the filtered dataframe
+    """
+    list_keys = options.keys()
+    result = df
+    for key in list_keys:
+      result = result.loc[result[key] == options.get(key)]
 
-    funding = []
-    if 'company_name' in options:
-      result = []
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          result.append(row)
-      csv_data = result
+    return result
+    
 
-    if 'city' in options:
-      result = []
-      for row in csv_data:
-        if row[4] == options['city']:
-          result.append(row)
-      csv_data = result
+#     if 'company_name' in options:
+#       result = []
+#       for row in csv_data:
+#         if row[1] == options['company_name']:
+#           result.append(row)
+#       csv_data = result
 
-    if 'state' in options:
-      result = []
-      for row in csv_data:
-        if row[5] == options['state']:
-          result.append(row)
-      csv_data = result
+#     if 'city' in options:
+#       result = []
+#       for row in csv_data:
+#         if row[4] == options['city']:
+#           result.append(row)
+#       csv_data = result
 
-    if 'round' in options:
-      result = []
-      for row in csv_data:
-        if row[9] == options['round']:
-          result.append(row)
-      csv_data = result
+#     if 'state' in options:
+#       result = []
+#       for row in csv_data:
+#         if row[5] == options['state']:
+#           result.append(row)
+#       csv_data = result
 
-    output = []
-    for row in csv_data:
-      mapped = {}
-      mapped['permalink'] = row[0]
-      mapped['company_name'] = row[1]
-      mapped['number_employees'] = row[2]
-      mapped['category'] = row[3]
-      mapped['city'] = row[4]
-      mapped['state'] = row[5]
-      mapped['funded_date'] = row[6]
-      mapped['raised_amount'] = row[7]
-      mapped['raised_currency'] = row[8]
-      mapped['round'] = row[9]
-      output.append(mapped)
+#     if 'round' in options:
+#       result = []
+#       for row in csv_data:
+#         if row[9] == options['round']:
+#           result.append(row)
+#       csv_data = result
 
-    return output
+#     output = []
+#     for row in csv_data:
+#       mapped = {}
+#       mapped['permalink'] = row[0]
+#       mapped['company_name'] = row[1]
+#       mapped['number_employees'] = row[2]
+#       mapped['category'] = row[3]
+#       mapped['city'] = row[4]
+#       mapped['state'] = row[5]
+#       mapped['funded_date'] = row[6]
+#       mapped['raised_amount'] = row[7]
+#       mapped['raised_currency'] = row[8]
+#       mapped['round'] = row[9]
+#       output.append(mapped)
 
-  @staticmethod
-  def find_by(options):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+#     return output
 
-    if 'company_name' in options:
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
+#   @staticmethod
+#   def find_by(options):
+#     with open("../startup_funding.csv", "rt") as csvfile:
+#       data = csv.reader(csvfile, delimiter=',', quotechar='"')
+#       # skip header
+#       next(data)
+#       csv_data = []
+#       for row in data:
+#         csv_data.append(row)
 
-    if 'city' in options:
-      for row in csv_data:
-        if row[4] == options['city']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
+#     if 'company_name' in options:
+#       for row in csv_data:
+#         if row[1] == options['company_name']:
+#           mapped = {}
+#           mapped['permalink'] = row[0]
+#           mapped['company_name'] = row[1]
+#           mapped['number_employees'] = row[2]
+#           mapped['category'] = row[3]
+#           mapped['city'] = row[4]
+#           mapped['state'] = row[5]
+#           mapped['funded_date'] = row[6]
+#           mapped['raised_amount'] = row[7]
+#           mapped['raised_currency'] = row[8]
+#           mapped['round'] = row[9]
+#           return mapped
 
-    if 'state' in options:
-      for row in csv_data:
-        if row[5] == options['state']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
+#     if 'city' in options:
+#       for row in csv_data:
+#         if row[4] == options['city']:
+#           mapped = {}
+#           mapped['permalink'] = row[0]
+#           mapped['company_name'] = row[1]
+#           mapped['number_employees'] = row[2]
+#           mapped['category'] = row[3]
+#           mapped['city'] = row[4]
+#           mapped['state'] = row[5]
+#           mapped['funded_date'] = row[6]
+#           mapped['raised_amount'] = row[7]
+#           mapped['raised_currency'] = row[8]
+#           mapped['round'] = row[9]
+#           return mapped
 
-    if 'round' in options:
-      for row in csv_data:
-        if row[9] == options['round']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
+#     if 'state' in options:
+#       for row in csv_data:
+#         if row[5] == options['state']:
+#           mapped = {}
+#           mapped['permalink'] = row[0]
+#           mapped['company_name'] = row[1]
+#           mapped['number_employees'] = row[2]
+#           mapped['category'] = row[3]
+#           mapped['city'] = row[4]
+#           mapped['state'] = row[5]
+#           mapped['funded_date'] = row[6]
+#           mapped['raised_amount'] = row[7]
+#           mapped['raised_currency'] = row[8]
+#           mapped['round'] = row[9]
+#           return mapped
 
-    raise RecordNotFound
+#     if 'round' in options:
+#       for row in csv_data:
+#         if row[9] == options['round']:
+#           mapped = {}
+#           mapped['permalink'] = row[0]
+#           mapped['company_name'] = row[1]
+#           mapped['number_employees'] = row[2]
+#           mapped['category'] = row[3]
+#           mapped['city'] = row[4]
+#           mapped['state'] = row[5]
+#           mapped['funded_date'] = row[6]
+#           mapped['raised_amount'] = row[7]
+#           mapped['raised_currency'] = row[8]
+#           mapped['round'] = row[9]
+#           return mapped
 
-class RecordNotFound(Exception):
-  pass
+#     raise RecordNotFound
+
+# class RecordNotFound(Exception):
+#   pass
 
 if __name__ == "__main__":
     
     print(FundingRaised.where({'company_name': 'Facebook'}))
-    print("___________________________________________________")
-    print(FundingRaised.find_by({'company_name': 'Facebook'}))
+    # print("___________________________________________________")
+    # print(FundingRaised.find_by({'company_name': 'Facebook'}))
+    
