01a13db0075ef29e3ab6c44375cc1e1c2dd75399
Franck <franck.somarribarodriguez@slu.edu>
2022-11-24 20:31:33 -0600

    Adjusted test to work with pandas dataframe, improve the find_by function by using pandas

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -12,96 +12,25 @@ class FundingRaised:
     """
     Filters the dataframe based on the parameters provided, and returns the filtered dataframe
     """
-    list_keys = options.keys()
     result = df
-    for key in list_keys:
+    for key in options.keys():
       result = result.loc[result[key] == options.get(key)]
 
     return result
     
 
-#   @staticmethod
-#   def find_by(options):
-#     with open("../startup_funding.csv", "rt") as csvfile:
-#       data = csv.reader(csvfile, delimiter=',', quotechar='"')
-#       # skip header
-#       next(data)
-#       csv_data = []
-#       for row in data:
-#         csv_data.append(row)
-
-#     if 'company_name' in options:
-#       for row in csv_data:
-#         if row[1] == options['company_name']:
-#           mapped = {}
-#           mapped['permalink'] = row[0]
-#           mapped['company_name'] = row[1]
-#           mapped['number_employees'] = row[2]
-#           mapped['category'] = row[3]
-#           mapped['city'] = row[4]
-#           mapped['state'] = row[5]
-#           mapped['funded_date'] = row[6]
-#           mapped['raised_amount'] = row[7]
-#           mapped['raised_currency'] = row[8]
-#           mapped['round'] = row[9]
-#           return mapped
-
-#     if 'city' in options:
-#       for row in csv_data:
-#         if row[4] == options['city']:
-#           mapped = {}
-#           mapped['permalink'] = row[0]
-#           mapped['company_name'] = row[1]
-#           mapped['number_employees'] = row[2]
-#           mapped['category'] = row[3]
-#           mapped['city'] = row[4]
-#           mapped['state'] = row[5]
-#           mapped['funded_date'] = row[6]
-#           mapped['raised_amount'] = row[7]
-#           mapped['raised_currency'] = row[8]
-#           mapped['round'] = row[9]
-#           return mapped
-
-#     if 'state' in options:
-#       for row in csv_data:
-#         if row[5] == options['state']:
-#           mapped = {}
-#           mapped['permalink'] = row[0]
-#           mapped['company_name'] = row[1]
-#           mapped['number_employees'] = row[2]
-#           mapped['category'] = row[3]
-#           mapped['city'] = row[4]
-#           mapped['state'] = row[5]
-#           mapped['funded_date'] = row[6]
-#           mapped['raised_amount'] = row[7]
-#           mapped['raised_currency'] = row[8]
-#           mapped['round'] = row[9]
-#           return mapped
-
-#     if 'round' in options:
-#       for row in csv_data:
-#         if row[9] == options['round']:
-#           mapped = {}
-#           mapped['permalink'] = row[0]
-#           mapped['company_name'] = row[1]
-#           mapped['number_employees'] = row[2]
-#           mapped['category'] = row[3]
-#           mapped['city'] = row[4]
-#           mapped['state'] = row[5]
-#           mapped['funded_date'] = row[6]
-#           mapped['raised_amount'] = row[7]
-#           mapped['raised_currency'] = row[8]
-#           mapped['round'] = row[9]
-#           return mapped
-
-#     raise RecordNotFound
-
-# class RecordNotFound(Exception):
-#   pass
+  @staticmethod
+  def find_by(options):
+    """
+    This function returs the first instance that is found based on the parameter sent
+    """
+    for key in options.keys():
+      result = df.loc[df[key] == options.get(key)].head(1)
+    return result
 
 if __name__ == "__main__":
     
     print(FundingRaised.where({'company_name': 'Facebook'}))
-    # print("___________________________________________________")
-    # print(FundingRaised.find_by({'company_name': 'Facebook'}))
+    print("___________________________________________________")
+    print(FundingRaised.find_by({'company_name': 'Facebook'}))
     
--- python/test.py
+++ python/test.py
@@ -26,26 +26,26 @@ def test_where_returns_events_by_type():
 def test_where_returns_no_events():
   assert len(FundingRaised.where({'company_name': 'NotFacebook'})) == 0
 
-# def test_find_by_event_by_company_name():
-#   row = FundingRaised.find_by({'company_name': 'Facebook'})
-#   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-#   values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-#   for i in range(0, len(keys)):
-#     assert row[keys[i]] == values[i]
+def test_find_by_event_by_company_name():
+  row = FundingRaised.find_by({'company_name': 'Facebook'})
+  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+  values = ['facebook', 'Facebook', '450.0', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
+  for i in range(0, len(keys)):
+    assert str(row[keys[i]].values[0]) == values[i]
 
-# def test_find_by_event_by_state():
-#   row = FundingRaised.find_by({'state': 'CA'})
-#   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-#   values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
-#   for i in range(0, len(keys)):
-#     assert row[keys[i]] == values[i]
+def test_find_by_event_by_state():
+  row = FundingRaised.find_by({'state': 'CA'})
+  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+  values = ['digg', 'Digg', '60.0', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
+  for i in range(0, len(keys)):
+    assert str(row[keys[i]].values[0]) == values[i]
 
 test_where_returns_events()
-# test_where_returns_correct_keys()
+test_where_returns_correct_keys()
 test_where_returns_events_by_city()
 test_where_returns_events_by_state()
 test_where_returns_events_by_company()
 test_where_returns_events_by_type()
 test_where_returns_no_events()
-# test_find_by_event_by_company_name()
-# test_find_by_event_by_state()
+test_find_by_event_by_company_name()
+test_find_by_event_by_state()
